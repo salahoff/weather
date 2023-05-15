807b3085cbb653fc453158260875e7ef15e01e05
Salahov Ainur <77484569+salahoff@users.noreply.github.com>
2023-05-15 14:07:24 +0300

    Add files via upload

---
+++ weather/weather/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.TemperatureBox = new System.Windows.Forms.TextBox();
+            this.DescriptionBox = new System.Windows.Forms.TextBox();
+            this.SpeedBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList3 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(46, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Показать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Введите название города:";
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(46, 52);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(179, 22);
+            this.NameBox.TabIndex = 2;
+            // 
+            // TemperatureBox
+            // 
+            this.TemperatureBox.Location = new System.Drawing.Point(46, 199);
+            this.TemperatureBox.Name = "TemperatureBox";
+            this.TemperatureBox.Size = new System.Drawing.Size(179, 22);
+            this.TemperatureBox.TabIndex = 3;
+            // 
+            // DescriptionBox
+            // 
+            this.DescriptionBox.Location = new System.Drawing.Point(46, 261);
+            this.DescriptionBox.Multiline = true;
+            this.DescriptionBox.Name = "DescriptionBox";
+            this.DescriptionBox.Size = new System.Drawing.Size(179, 97);
+            this.DescriptionBox.TabIndex = 4;
+            // 
+            // SpeedBox
+            // 
+            this.SpeedBox.Location = new System.Drawing.Point(46, 392);
+            this.SpeedBox.Name = "SpeedBox";
+            this.SpeedBox.Size = new System.Drawing.Size(179, 22);
+            this.SpeedBox.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Температура:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Описание:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 372);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Скорость ветра:";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList2
+            // 
+            this.imageList2.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList2.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // imageList3
+            // 
+            this.imageList3.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList3.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList3.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.SpeedBox);
+            this.Controls.Add(this.DescriptionBox);
+            this.Controls.Add(this.TemperatureBox);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Погода";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox TemperatureBox;
+        private System.Windows.Forms.TextBox DescriptionBox;
+        private System.Windows.Forms.TextBox SpeedBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList3;
+    }
+}
+
---
+++ weather/weather/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace weather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Info //В этом классе прописываем нужные нам переменные с соответствующими типами данных
+        {
+            public float Temp { get; set; } //Температура
+            public string Description { get; set; } //Описание
+            public float Speed { get; set; } //Скорость ветра     
+        }
+
+        public class WeatherResponse //В этот класс будем записываться ответ c сервера
+        {
+            public Info Main { get; set; }
+            public Info[] Weather { get; set; }
+            public Info Wind { get; set; }               
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Обрабатываем нажатие на кнопку "Показать" 
+        {
+
+            string city = NameBox.Text; //Записываем в переменную название города, полученную из поля ввода
+            string response; //Переменная для чтения файла
+
+            if (NameBox.Text != "") //Проверяем поле ввода на пустое значение
+            {
+
+                string url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=c9d9e13aa3174bb31933dbb96d7bb525"; // Создаем запрос {ссылка + название города + ключ}
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url); //Посылаем запрос на сервер
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse(); //Получаем ответ от сервера {файл формата JSON}
+
+
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()) )//Созадаем поток для чтения файла
+                {
+                    response = streamReader.ReadToEnd(); //Читаем файл
+                }
+
+                WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response); //Записываем в переменную данные из прочитанного файла
+
+
+                TemperatureBox.Text = weatherResponse.Main.Temp.ToString() + " °C"; //Выводим температуру
+                DescriptionBox.Text = weatherResponse.Weather[0].Description.ToString().ToUpper(); //Выводим описание
+                SpeedBox.Text = weatherResponse.Wind.Speed.ToString() + " m/s"; //Выводим скорость ветра
+
+            }
+
+                else MessageBox.Show("Введите название города!", "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error); //Выводим сообщение об ошибке
+        }
+    }
+}
